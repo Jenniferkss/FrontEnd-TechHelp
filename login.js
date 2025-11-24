716c5f4140a48e979325e7f636f3ab772bfce0e1
Ana Julia <ana.j.ramos9@aluno.senai.br>
2025-11-24 16:22:29 -0300

    feat:finaliza pagina de login com alteração de tema

--- login.js
+++ login.js
@@ -1,19 +1,12 @@
-// Selecionando o botão de alternância
-const toggleButton = document.getElementById('toggle-mode');
+const btn = document.getElementById('themeBtn');
+const img = document.getElementById('themeImg');
 
-// Verificando o estado inicial do modo
-const darkMode = localStorage.getItem('dark-mode') === 'true';
-
-// Aplicando o modo escuro se necessário
-if (darkMode) {
-  document.body.classList.add('dark-mode');
-}
-
-// Função para alternar o modo claro e escuro
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-
-  // Salvando a preferência do usuário no localStorage
-  const isDarkMode = document.body.classList.contains('dark-mode');
-  localStorage.setItem('dark-mode', isDarkMode);
+btn.addEventListener('click', () => {
+  if (btn.textContent === 'Dark Theme') {
+    btn.textContent = 'Light Theme';
+    img.src = '../assets/img/Escola-Dia.png';
+  } else {
+    btn.textContent = 'Dark Theme';
+    img.src = '../assets/img/Escola-Noite.png';
+  }
 });
